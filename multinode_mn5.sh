b540f006f4034c10c5fb3cc0485d4da994a1a0b0
Tomas Ockier <tomiock>
2025-12-09 23:10:23 +0100

    [run] test 32 node

--- multinode_mn5.sh
+++ multinode_mn5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -D .
-#SBATCH --ntasks=2
-#SBATCH --nodes=2
+#SBATCH --ntasks=32
+#SBATCH --nodes=32
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=80
 #SBATCH --time=0:10:00
@@ -74,7 +74,7 @@ wandb offline
 
 # *****
 NGPUS=4
-NNODES=2
+NNODES=32
 # *****
 
 srun --cpu-bind=none torchrun --nproc_per_node=$NGPUS \
